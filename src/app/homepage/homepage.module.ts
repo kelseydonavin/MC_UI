6653394804adf5bf93f98d7c9330b8584315c5b7
Trenton Fales <trentafales@yahoo.com>
2019-10-13 18:38:07 -0700

    Add menu component

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from '../homepage/homepage.component';
+import { MenuComponentComponent } from './menu-component/menu-component.component';
 
 
 
 @NgModule({
-  declarations: [HomepageComponent],
+  declarations: [HomepageComponent, MenuComponentComponent],
   imports: [
     CommonModule
   ],
