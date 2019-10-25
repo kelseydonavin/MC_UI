b334a0b20e5f8293cafcea87d2d7a4efb57aa514
Trenton Fales <trentafales@yahoo.com>
2019-10-24 20:41:55 -0700

    17 - Update menu to new design

--- src/app/create-login-page/create-login-page.module.ts
+++ src/app/create-login-page/create-login-page.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateLoginPageComponent } from './create-login-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
-
-
+import { HomepageModule } from '../homepage/homepage.module';
 
 @NgModule({
   declarations: [CreateLoginPageComponent, CreatePageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HomepageModule
   ],
   exports: [CreateLoginPageComponent, CreatePageComponent]
 })
--- src/app/metrics-page/metrics-page.module.ts
+++ src/app/metrics-page/metrics-page.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MetricsPageComponent } from './metrics-page.component';
-
-
+import { HomepageModule } from '../homepage/homepage.module';
 
 @NgModule({
-  declarations: [MetricsPageComponent],
-  imports: [
-    CommonModule
+  declarations: [
+    MetricsPageComponent
   ],
-  exports: [MetricsPageComponent]
+  imports: [
+    CommonModule,
+    HomepageModule
+  ],
+  exports: [
+    MetricsPageComponent
+  ]
 })
 export class MetricsPageModule { }
